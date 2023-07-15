6361c13f707aba08483ad69a0604a060716d3be8
Teo <teodorescuteofil@gmail.com>
2023-07-15 17:32:45 +0300

    Update registration.py

--- registration.py
+++ registration.py
@@ -17,19 +17,23 @@ class Employee():
             Register an employee in the database.
 
             """
-            if self.idManager == 'CEO':
-                cursor.execute(f"insert into employees values (null, '{wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")
-                conn.commit()
-                print('You have successfully registered')
-            
-            cursor.execute('select * from employees')
-            rows = cursor.fetchall()
-            
-            for row in rows:
-                if self.idManager == str(row[0]):
-                    if wUpper(self.company) == row[3]:
-                        cursor.execute(f"insert into employees values (null, '{wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")   
-                        conn.commit()
-                        print('You have successfully registered')
+            try:
+                if self.idManager == 'CEO':
+                    cursor.execute(f"insert into employees values (null, {wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")
+                    conn.commit()
+                    print('You have successfully registered')
+                
+                cursor.execute('select * from employees')
+                rows = cursor.fetchall()
+                
+                for row in rows:
+                    if self.idManager == str(row[0]):
+                        if wUpper(self.company) == row[3]:
+                            cursor.execute(f"insert into employees values (null, '{wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")   
+                            conn.commit()
+                            print('You have successfully registered')
+
+            except mysql.connector.errors.ProgrammingError:
+                print('Please enter only letters')
 
         registration(self)
