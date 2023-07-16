18aba6ef60b2b6f1eb118e2d7b9cb5aefc6da371
Teo <teodorescuteofil@gmail.com>
2023-07-16 15:50:01 +0300

    update registration function

--- registration.py
+++ registration.py
@@ -16,16 +16,17 @@ class Employee():
             """
             Register an employee in the database.
 
+            If there is an exception of type 'mysql.connector.errors.ProgrammingError', it means that the input contains non-letter characters.
             """
             try:
                 if self.idManager == 'CEO':
-                    cursor.execute(f"insert into employees values (null, {wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")
+                    cursor.execute(f"insert into employees values (null, '{wUpper(self.fName)}', '{wUpper(self.lName)}', '{wUpper(self.company)}', '{self.idManager}');")
                     conn.commit()
                     print('You have successfully registered')
-                
+                    
                 cursor.execute('select * from employees')
                 rows = cursor.fetchall()
-                
+                    
                 for row in rows:
                     if self.idManager == str(row[0]):
                         if wUpper(self.company) == row[3]:
