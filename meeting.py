259a7c10ff801c666f911beade9269dd87dabc91
Teo <131667175+teofil11@users.noreply.github.com>
2023-07-16 16:12:06 +0300

    Update meeting.py

--- meeting.py
+++ meeting.py
@@ -1,6 +1,15 @@
 from functions.sendmail import send_email
 
 def meet_email(list_of_persons,subject,content):
+    """
+    Sends an email to multiple recipients.
+
+    Args:
+        list_of_persons (list): A list of email addresses of the recipients.
+        subject (str): The subject of the email.
+        content (str): The content of the email.
+    """
+    
     email = 'teodorescu_teofil@yahoo.com'
     for person in list_of_persons:
         send_email(email,subject,content)
