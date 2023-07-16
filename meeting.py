e8d38e4cf274e46528842036ebcf0ffe4b228f1a
Teo <131667175+teofil11@users.noreply.github.com>
2023-07-16 19:48:51 +0300

    Update meeting.py

--- meeting.py
+++ meeting.py
@@ -2,10 +2,10 @@ from functions.sendmail import send_email
 
 def meet_email(list_of_persons,subject,content):
     """
-    Sends an email to multiple recipients.
+    Sends an email to multiple persons.
 
     Args:
-        list_of_persons (list): A list of email addresses of the recipients.
+        list_of_persons (list): A  list of ids of the persons.
         subject (str): The subject of the email.
         content (str): The content of the email.
     """
